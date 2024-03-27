624a2c6feaa12de8068a2468bc44586731f32cc7
Saklyfiras <133059715+SaklyFiras1@users.noreply.github.com>
2024-03-27 02:37:41 +0100

    Update UserController.java

--- backend/src/main/java/com/application/controller/UserController.java
+++ backend/src/main/java/com/application/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.application.model.Chapter;
 import com.application.model.Enrollment;
 import com.application.model.Professor;
